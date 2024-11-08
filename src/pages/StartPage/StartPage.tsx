3e6d022569296d44f0316faba162e270f780ea38
Shaqs0 <hhgfhhhggb@gmail.com>
2024-11-09 00:03:54 +0300

    first commit

---
+++ src/pages/StartPage/StartPage.tsx
@@ -0,0 +1,11 @@
+
+
+export function StartPage() {
+	return (
+		<div className="flex h-screen w-screen items-center justify-center bg-black">
+			<div className='text-3xl text-white'>
+            Hello world
+			</div>
+		</div>
+	);
+}
