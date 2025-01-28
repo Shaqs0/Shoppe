c80520b267e52f94f2f0be59c368dc0da6c247c1
Shaqs0 <hhgfhhhggb@gmail.com>
2025-01-28 23:47:44 +0300

    first commit

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,13 @@
+export function Layout () {
+	return (
+		<div className="">
+			<header className="">
+				<p>Start</p>
+			</header>
+			<main></main>
+			<footer>
+                
+			</footer>
+		</div>
+	);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}', 
+		
+	],
+	theme: {
+		colors: {
+			'primary-orange': '#FFA034'
+		},
+		extend: {
+			
+		},
+	},
+	plugins: [],
+};
