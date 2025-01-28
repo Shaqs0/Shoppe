0fbe578d2a80c80e246eee9451c1b90b60c1fc2e
Shaqs0 <hhgfhhhggb@gmail.com>
2025-01-29 01:00:52 +0300

    add header

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export {default as Logo} from './header/logo.svg';
+export {default as searchIcon} from './header/search.svg';
+export {default as cartIcon} from './header/cart.svg';
+export {default as profileIcon} from './header/profile.svg';
+export {default as sepLine} from './header/line.svg';
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,34 @@
+import { Outlet } from 'react-router-dom';
+import { cartIcon, Logo, profileIcon, searchIcon, sepLine } from '../assets';
+
 export function Layout () {
 	return (
 		<div className="">
-			<header className="">
-				<p>Start</p>
+			<header className="mt-[72px] flex w-[1248px] items-center justify-between">
+				<div>
+					<img src={Logo}/>
+				</div>
+				<div className='flex'>
+					<div className='mr-12'>
+						<ul className='flex cursor-pointer gap-16'>
+							<li className=''>Shop</li>
+							<li className=''>Blog</li>
+							<li className=''>Our Story</li>
+						</ul>
+					</div>
+					<img src={sepLine}/>
+					<div className='ml-12'>
+						<div className='flex cursor-pointer items-center gap-[39px]'>
+							<img src={searchIcon} className='size-[19px]'/>
+							<img src={cartIcon} className='size-[21px]'/>
+							<img src={profileIcon} className='size-[20px]'/>
+						</div>
+					</div>
+				</div>
 			</header>
-			<main></main>
+			<main>
+				<Outlet/>
+			</main>
 			<footer>
                 
 			</footer>
